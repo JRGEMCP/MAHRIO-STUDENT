a5f07a4166894d71b9be85dc8a8806833cbfadd7
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-10-19 23:16:46 -0700

    milestone fix

--- src/app/components/milestone-panel/milestone-panel.component.js
+++ src/app/components/milestone-panel/milestone-panel.component.js
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 import template from './milestone-panel.template.html';
+import style from './milestone-panel.style.scss';
 
 @Component({
   selector: 'milestone-panel',
   template,
+  styles: [style],
   inputs: ['active', 'id']
 })
 
